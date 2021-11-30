3335048b03c753922f8c1a6907135c36c6d182a3
KiriKaz <igkepsi@gmail.com>
2021-11-30 20:05:50 -0300

    Initial commit, slicing down template code.

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Answer: Board the Great Eastern Sun, and find out yourself, silly.
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Posts: NextPage = () => {
+  return (
+    <div>
+      When does the Black Moon howl?
+    </div>
+  );
+};
+
+export default Posts;
