48b4a4d66d769331b577a0c59012434f19d6beee
KiriKaz <igkepsi@gmail.com>
2021-11-30 21:11:56 -0300

    feat: display all posts on posts route

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,58 @@
-import type { NextPage } from 'next';
+import React from "react";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Link from "next/link";
 
-const Posts: NextPage = () => {
+import fs from 'fs';
+import { v4 as uuid } from 'uuid';
+
+
+interface Post {
+  rawContent: any,
+  id: any,
+  slug: string,
+  title: string
+}  // Define this type elsewhere -- probably in the entity or smth. Depends on backend, especially if using ORM.
+
+interface IPostsProps {
+  posts: Post[]
+}
+
+export const getStaticProps: GetStaticProps<IPostsProps> = async () => {
+  // Yoinked from a bunch of places. TODO
+
+  const files = fs.readdirSync(`${process.cwd()}/contents`, "utf-8");  // Change later! also, process.cwd? I don't like it.
+
+  const posts: Post[] = files
+    .filter((filename: string) => filename.endsWith(".md"))  // Potentially use different format. (no we're not.)
+    .map((filename: string) => {
+      const path = `${process.cwd()}/contents/${filename}`;
+      const rawContent = fs.readFileSync(path, {
+        encoding: "utf-8",
+      });
+
+      return { rawContent, id: uuid(), slug: 'TODO', title: 'TODO' };
+    });
+
+  return {
+    props: { posts },
+  };
+};
+
+const Posts = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
-      When does the Black Moon howl?
+      <h1>List of posts</h1>
+      <ul>
+        {posts.map((post, idx) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
