8e0f88cff360ae5939e6956478d147f5e666007e
KiriKaz <igkepsi@gmail.com>
2021-12-04 09:17:55 -0300

    chore: Add random favicon
    Pulled from favicon dot io, just using the emoji for a door as of right now.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import theme from '../styles/theme';
 import { ThemeProvider } from '@emotion/react';
@@ -8,6 +9,13 @@ import { ThemeProvider } from '@emotion/react';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Wander the Halls</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,6 @@ import Container from '@mui/material/Container';
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Wander the Halls</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Container maxWidth="sm">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta architecto aperiam corrupti, facere laborum animi cupiditate tenetur voluptatem dolore, ducimus sed quidem, totam facilis natus! Eius, quo! Praesentium, hic quaerat?
           Mollitia non voluptas fugit maxime reiciendis sed pariatur sapiente, ullam nobis soluta quasi exercitationem distinctio obcaecati cum aliquid consectetur sequi nam, accusamus autem excepturi explicabo nihil. Quia vitae cupiditate possimus?</p>
